d6ccce046d4da59826063282da94bd6bd7e3e286
arpanpathak <arpanpathak1996@gmail.com>
2017-10-13 00:21:17 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var animationComplete = false;
+function animationCalled() {
+	animationComplete = true;
+}
+function showClock(){
+	$('.clock').fadeIn('slow');
+}
+$(function(){
+	$('svg').on('load',(function () {
+	      setTimeout(function () {
+	          if (!animationComplete) {
+	              animateT();
+	          }
+	      }, 1000 * 12
+	   );
+	   }) );
+
+	var date = new Date("March 5, 2018 02:15:00"); //Month Days, Year HH:MM:SS
+	var now = new Date();
+	var diff = (date.getTime()/1000) - (now.getTime()/1000);
+
+	var clock = $('.clock').FlipClock(diff,{
+	    clockFace: 'DailyCounter',
+	    countdown: true
+	});
+			   
+	clock.start();   
+});
